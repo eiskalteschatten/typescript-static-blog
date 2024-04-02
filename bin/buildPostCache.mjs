bca0e2ffb44e63688d323e3bf885f72bea0b0b91
Alex Seifert <alex@alexseifert.com>
2024-04-02 13:24:52 +0200

    Finish the all posts cache

--- bin/buildPostCache.mjs
+++ bin/buildPostCache.mjs
@@ -1,6 +1,8 @@
 import path from 'node:path';
 import fs from 'node:fs';
 
+console.log('Building post cache...');
+
 const cacheDirectory = path.resolve(process.cwd(), '.cache');
 const postsDirectory = path.resolve(process.cwd(), 'data', 'posts');
 const postFolders = await fs.promises.readdir(postsDirectory);
@@ -21,11 +23,13 @@ for (const postFolder of postFolders) {
 }
 
 allPostsData.sort((a, b) => a.publishedDate > b.publishedDate ? -1 : 1);
+const sortedPostFolders = allPostsData.map(post => post.id);
 
 if (!fs.existsSync(cacheDirectory)) {
   fs.mkdirSync(cacheDirectory);
 }
 
 const cacheFile = path.resolve(cacheDirectory, 'allPosts.json');
+fs.writeFileSync(cacheFile, JSON.stringify(sortedPostFolders, null, 2));
 
-// TODO: Do I write all post data to the cache or just the directory names?
+console.log('Post cache built.');
