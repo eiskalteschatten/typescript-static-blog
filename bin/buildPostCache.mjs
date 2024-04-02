2612a4775acb596c561398b3fd79a47d34942f77
Alex Seifert <alex@alexseifert.com>
2024-04-02 13:29:23 +0200

    Start preparing the script to handle the other caches

--- bin/buildPostCache.mjs
+++ bin/buildPostCache.mjs
@@ -1,35 +1,60 @@
 import path from 'node:path';
 import fs from 'node:fs';
 
-console.log('Building post cache...');
+console.log('Building post caches...');
 
-const cacheDirectory = path.resolve(process.cwd(), '.cache');
 const postsDirectory = path.resolve(process.cwd(), 'data', 'posts');
-const postFolders = await fs.promises.readdir(postsDirectory);
 const allPostsData = [];
-
-for (const postFolder of postFolders) {
-  const fullPathToFolder = path.resolve(postsDirectory, postFolder);
-  const fullPathtoMetaFile = path.resolve(fullPathToFolder, 'meta.json');
-  const fileContent = await fs.promises.readFile(fullPathtoMetaFile, 'utf8');
-
-  try {
-    const postData = JSON.parse(fileContent);
-    allPostsData.push(postData);
-  }
-  catch (error) {
-    console.error(error);
-  }
-}
-
-allPostsData.sort((a, b) => a.publishedDate > b.publishedDate ? -1 : 1);
-const sortedPostFolders = allPostsData.map(post => post.id);
+const cacheDirectory = path.resolve(process.cwd(), '.cache');
 
 if (!fs.existsSync(cacheDirectory)) {
   fs.mkdirSync(cacheDirectory);
 }
 
-const cacheFile = path.resolve(cacheDirectory, 'allPosts.json');
-fs.writeFileSync(cacheFile, JSON.stringify(sortedPostFolders, null, 2));
+async function setup() {
+  const postFolders = await fs.promises.readdir(postsDirectory);
 
-console.log('Post cache built.');
+  for (const postFolder of postFolders) {
+    const fullPathToFolder = path.resolve(postsDirectory, postFolder);
+    const fullPathtoMetaFile = path.resolve(fullPathToFolder, 'meta.json');
+    const fileContent = await fs.promises.readFile(fullPathtoMetaFile, 'utf8');
+
+    try {
+      const postData = JSON.parse(fileContent);
+      allPostsData.push(postData);
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  allPostsData.sort((a, b) => a.publishedDate > b.publishedDate ? -1 : 1);
+}
+
+function buildAllPostsCache() {
+  console.log('Building all posts cache...');
+
+  const cacheFile = path.resolve(cacheDirectory, 'allPosts.json');
+  const sortedPostFolders = allPostsData.map(post => post.id);
+  fs.writeFileSync(cacheFile, JSON.stringify(sortedPostFolders, null, 2));
+}
+
+function buildCategoryCache() {
+  console.log('Building category cache...');
+}
+
+function buildTagCache() {
+  console.log('Building tag cache...');
+}
+
+function buildArchiveCache() {
+  console.log('Building archive cache...');
+}
+
+await setup();
+buildAllPostsCache();
+buildCategoryCache();
+buildTagCache();
+buildArchiveCache();
+
+console.log('All post caches have been successfully built.');
