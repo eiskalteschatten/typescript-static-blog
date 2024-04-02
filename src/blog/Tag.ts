9538ac7d4738b1f1e124d5e428d2b1518913a11e
Alex Seifert <alex@alexseifert.com>
2024-04-02 17:45:24 +0200

    Use the Tags class

--- src/blog/Tag.ts
+++ src/blog/Tag.ts
@@ -3,6 +3,8 @@ import fs from 'node:fs';
 
 import { CachedTag } from '@/interfaces/tag.interface';
 
+import Tags from './Tags';
+
 export default class Tag {
   private cacheDirectory = path.resolve(process.cwd(), '.cache', 'tags');
 
@@ -11,16 +13,8 @@ export default class Tag {
   constructor(private tagSlug: string) {}
 
   async getTagMetaData(): Promise<CachedTag> {
-    const cacheFile = path.resolve(this.cacheDirectory, 'allTags.json');
-    const fileContent = await fs.promises.readFile(cacheFile, 'utf8');
-
-    try {
-      const tags = JSON.parse(fileContent) as CachedTag[];
-      this.metaData = tags.find(tag => tag.slug === this.tagSlug);
-      return this.metaData;
-    }
-    catch (error) {
-      console.error(error);
-    }
+    const tags = await Tags.getAllTags();
+    this.metaData = tags.find(tag => tag.slug === this.tagSlug);
+    return this.metaData;
   }
 }
