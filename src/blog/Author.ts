ec282bfb14de7a1779a2ed8c5fd77826fa044854
Alex <alex@alexseifert.com>
2024-04-12 22:14:01 +0200

    Start the Author class

---
+++ src/blog/Author.ts
@@ -0,0 +1,14 @@
+import authors from '@data/authors.json';
+
+import { AuthorMetaData } from '@/interfaces/author.interface';
+
+export default class Author {
+  metaData: AuthorMetaData;
+
+  constructor(public authorId: string) {}
+
+  async getAuthorMetaData(): Promise<AuthorMetaData> {
+    this.metaData = authors.find(author => author.id === this.authorId);
+    return this.metaData;
+  }
+}
