12c8e4e56177df99214e5c9e939b25f00c00bd7d
Alex <alex@alexseifert.com>
2024-04-13 13:40:33 +0200

    The api endpoint for posts now works

--- src/routes/api/posts/index.ts
+++ src/routes/api/posts/index.ts
@@ -3,9 +3,10 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import BlogPostIndex from '@/blog/BlogPostIndex';
 
 export default async (app: FastifyInstance) => {
-  type PostsRequest = FastifyRequest<{ Querystring: { page?: number; numberOfPosts?: number } }>;
+  type PostsRequest = FastifyRequest<{ Querystring: { page?: number; postsPerPage?: number } }>;
   app.get('/', async (req: PostsRequest, reply: FastifyReply) => {
-    const blogPostIndex = new BlogPostIndex(req.query.page, req.query.numberOfPosts);
+    const postsPerPage = req.query.postsPerPage ? Number(req.query.postsPerPage) + 1 : undefined;
+    const blogPostIndex = new BlogPostIndex(req.query.page, postsPerPage);
     const posts = await blogPostIndex.getAllPosts();
     return reply.send(posts);
   });
