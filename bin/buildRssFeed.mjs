19ea74e674b490aff930a421b9bfd2a8fea3348c
Alex <alex@alexseifert.com>
2024-04-14 11:29:19 +0200

    Almost finish the RSS feed

--- bin/buildRssFeed.mjs
+++ bin/buildRssFeed.mjs
@@ -2,11 +2,11 @@ import fs from 'node:fs';
 import path from 'node:path';
 import RSS from 'rss';
 
+import categories from '../data/categories.json' assert { type: 'json' };
 import authors from '../data/authors.json' assert { type: 'json' };
 
 console.log('Building the RSS feed...');
 
-const postsDirectory = path.resolve(process.cwd(), 'data', 'posts');
 const url = 'http://localhost:4000';
 
 const feed = new RSS({
@@ -18,6 +18,37 @@ const feed = new RSS({
   language: 'en-US',
   pubDate: new Date().toISOString(),
 });
+
+const cacheFile = path.resolve(this.cacheDirectory, 'allPosts.json');
+
+if (!fs.existsSync(cacheFile)) {
+  throw new Error('Please run npm run post:cache before building the RSS feed!');
+}
+
+const fileContent = await fs.promises.readFile(cacheFile, 'utf8');
+const postIds = JSON.parse(fileContent);
+
+const postIdsForPage = postIds.slice((this.page - 1) * this.postsPerPage, this.page * this.postsPerPage);
+
+for (const postId of postIdsForPage) {
+  const fullPathToFolder = path.resolve(this.postsDirectory, postId);
+  const fullPathtoMetaFile = path.resolve(fullPathToFolder, 'meta.json');
+  const fileContent = await fs.promises.readFile(fullPathtoMetaFile, 'utf8');
+  const postMetaData = JSON.parse(fileContent);
+
+  if (new Date(postMetaData.publishedDate) <= new Date() && postMetaData.status === 'published') {
+    const postCategories = categories.filter(category => postMetaData.categories.includes(category.id));
+
+    feed.item({
+      title: postMetaData.title,
+      description: contentHtml,
+      url: `${url}/post/${postMetaData.id}`,
+      categories: postCategories.map(category => category.name),
+      author: authors.find(author => author.id === postMetaData.authors[0])?.name ?? '',
+      date: postMetaData.publishedDate,
+    });
+  }
+}
 
 const xml = feed.xml();
 const writeFile = path.resolve(process.cwd(), 'public', 'feed.xml');
