1fc68e4180a3a21853a7a98f16d642f0c5227e26
Alex Seifert <alex@alexseifert.com>
2024-04-14 12:13:00 +0200

    Add the sitemap for tags

--- src/routes/sitemap/index.ts
+++ src/routes/sitemap/index.ts
@@ -3,6 +3,7 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import BlogPostIndex from '@/blog/BlogPostIndex';
 import BlogPost from '@/blog/BlogPost';
 import Categories from '@/blog/Categories';
+import Tags from '@/blog/Tags';
 
 const url = 'http://localhost:4000';
 
@@ -45,6 +46,27 @@ export default async (app: FastifyInstance) => {
       xml += `
         <url>
           <loc>${url}/category/${category.id}/</loc>
+          <lastmod>${new Date().toISOString()}</lastmod>
+        </url>
+      `;
+    }
+
+    xml += '</urlset>';
+
+    return reply
+      .header('Content-Type', 'application/rss+xml; charset=UTF-8')
+      .send(xml);
+  });
+
+  app.get('/tags', async (req: FastifyRequest, reply: FastifyReply) => {
+    let xml = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
+
+    const tags = await Tags.getAllTags();
+
+    for (const tag of tags) {
+      xml += `
+        <url>
+          <loc>${url}/tag/${tag.slug}/</loc>
           <lastmod>${new Date().toISOString()}</lastmod>
         </url>
       `;
