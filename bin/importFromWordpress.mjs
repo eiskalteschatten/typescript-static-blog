9e5f3225d22c5b0628f26cd37a1f5089aa0299d0
Alex Seifert <alex@alexseifert.com>
2024-04-14 13:06:50 +0200

    Start the author import

--- bin/importFromWordpress.mjs
+++ bin/importFromWordpress.mjs
@@ -1,11 +1,3 @@
-console.log('Importing posts from Wordpress...');
-
-const apiUrl = 'https://www.developers-notebook.com/wp-json/wp/v2/';
-const authorsUrl = `${apiUrl}users`;
-const categoriesUrl = `${apiUrl}categories`;
-const tagsUrl = `${apiUrl}tags`;
-const postsUrl = `${apiUrl}posts`;
-
 /*
   1. Import categories, tags and authors (and author avatars) -- only those that have posts
   2. Import posts with images and link them to the categories, tags and authors
@@ -14,5 +6,41 @@ const postsUrl = `${apiUrl}posts`;
     - Think about pagination
     - Think about post status
 */
+
+import fs from 'node:fs';
+import path from 'node:path';
+
+console.log('Importing posts from Wordpress...');
+
+const dataDirectory = path.resolve(process.cwd(), 'data');
+
+const apiUrl = 'https://www.developers-notebook.com/wp-json/wp/v2/';
+const authorsUrl = `${apiUrl}users`;
+const categoriesUrl = `${apiUrl}categories`;
+const tagsUrl = `${apiUrl}tags`;
+const postsUrl = `${apiUrl}posts`;
+
+async function fetchAuthors() {
+  const response = await fetch(authorsUrl);
+  const authors = await response.json();
+
+  // TODO: save author avatars
+
+  const newAuthors = authors.map(author => {
+    return {
+      id: author.slug,
+      name: author.name,
+      bio: author.description,
+      website: author.url,
+      // TODO!
+      avatar: author.avatar_urls['96'],
+    };
+  });
+
+  const authorsFile = path.resolve(dataDirectory, 'authors.json');
+  await fs.promises.writeFile(authorsFile, JSON.stringify(newAuthors, null, 2));
+}
+
+await fetchAuthors();
 
 console.log('Posts successfully imported from Wordpress!');
