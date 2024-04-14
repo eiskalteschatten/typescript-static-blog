178acf6145e1cceeccd1ba611a460fd6cfb9a4c7
Alex Seifert <alex@alexseifert.com>
2024-04-14 12:11:45 +0200

    Add the sitemap for categories

--- src/routes/sitemap/index.ts
+++ src/routes/sitemap/index.ts
@@ -2,11 +2,12 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 
 import BlogPostIndex from '@/blog/BlogPostIndex';
 import BlogPost from '@/blog/BlogPost';
+import Categories from '@/blog/Categories';
+
+const url = 'http://localhost:4000';
 
 export default async (app: FastifyInstance) => {
   app.get('/posts', async (req: FastifyRequest, reply: FastifyReply) => {
-    const url = 'http://localhost:4000';
-
     let xml = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
 
     const blogPostIndex = new BlogPostIndex();
@@ -21,11 +22,32 @@ export default async (app: FastifyInstance) => {
 
         xml += `
           <url>
-            <loc>${url}/post/${blogPost.metaData.id}</loc>
+            <loc>${url}/post/${blogPost.metaData.id}/</loc>
             <lastmod>${lastmod.toISOString()}</lastmod>
           </url>
         `;
       }
+    }
+
+    xml += '</urlset>';
+
+    return reply
+      .header('Content-Type', 'application/rss+xml; charset=UTF-8')
+      .send(xml);
+  });
+
+  app.get('/categories', async (req: FastifyRequest, reply: FastifyReply) => {
+    let xml = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
+
+    const categories = Categories.getSorted();
+
+    for (const category of categories) {
+      xml += `
+        <url>
+          <loc>${url}/category/${category.id}/</loc>
+          <lastmod>${new Date().toISOString()}</lastmod>
+        </url>
+      `;
     }
 
     xml += '</urlset>';
