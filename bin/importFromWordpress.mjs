53b1d386272a092c8e5f8e08ed47ce2af61d46db
Alex Seifert <alex@alexseifert.com>
2024-04-14 13:01:37 +0200

    Define the API URLs

--- bin/importFromWordpress.mjs
+++ bin/importFromWordpress.mjs
@@ -1,5 +1,11 @@
 console.log('Importing posts from Wordpress...');
 
+const apiUrl = 'https://www.developers-notebook.com/wp-json/wp/v2/';
+const authorsUrl = `${apiUrl}users`;
+const categoriesUrl = `${apiUrl}categories`;
+const tagsUrl = `${apiUrl}tags`;
+const postsUrl = `${apiUrl}posts`;
+
 /*
   1. Import categories, tags and authors (and author avatars) -- only those that have posts
   2. Import posts with images and link them to the categories, tags and authors
