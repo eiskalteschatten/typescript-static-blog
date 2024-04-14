5b676069349080e3823e0fd11c310b6d9e565ee8
Alex Seifert <alex@alexseifert.com>
2024-04-14 12:57:41 +0200

    Come up with a plan for the importer

--- bin/importFromWordpress.mjs
+++ bin/importFromWordpress.mjs
@@ -1,3 +1,12 @@
 console.log('Importing posts from Wordpress...');
 
+/*
+  1. Import categories, tags and authors (and author avatars) -- only those that have posts
+  2. Import posts with images and link them to the categories, tags and authors
+
+  Notes:
+    - Think about pagination
+    - Think about post status
+*/
+
 console.log('Posts successfully imported from Wordpress!');
