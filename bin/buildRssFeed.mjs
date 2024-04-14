a10c1fc9a6c40b800f5b171b1907ca6ab3f6a9c6
Alex <alex@alexseifert.com>
2024-04-14 11:16:56 +0200

    Start the script for building the RSS feed

--- bin/buildRssFeed.mjs
+++ bin/buildRssFeed.mjs
@@ -4,10 +4,24 @@ import RSS from 'rss';
 
 import authors from '../data/authors.json' assert { type: 'json' };
 
-const postsDirectory = path.resolve(process.cwd(), 'data', 'posts');
-
 console.log('Building the RSS feed...');
 
+const postsDirectory = path.resolve(process.cwd(), 'data', 'posts');
+const url = 'http://localhost:4000';
 
+const feed = new RSS({
+  title: 'TypeScript Static Blog',
+  description: 'A simple blog written in TypeScript that is generated statically.',
+  'feed_url': `${url}/feed.xml`,
+  'site_url': `${url}`,
+  copyright: 'Copyright (c) Alex Seifert',
+  language: 'en-US',
+  pubDate: new Date().toISOString(),
+});
+
+const xml = feed.xml();
+const writeFile = path.resolve(process.cwd(), 'public', 'feed.xml');
+
+fs.writeFileSync(writeFile, xml);
 
 console.log('The RSS feed has been successfully built.');
