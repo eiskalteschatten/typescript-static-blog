9dcdf5f26cad6fbb80a48320aaa1a49f27821872
Alex Seifert <alex@alexseifert.com>
2024-04-15 12:38:13 +0200

    Start the homepage

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,23 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 
+import BlogPostIndex from '@/blog/BlogPostIndex';
+import Sidebar from '@/components/Sidebar';
+
 export default async (app: FastifyInstance) => {
   app.get('/', async (req: FastifyRequest, reply: FastifyReply) => {
+    const blogPostIndex = new BlogPostIndex();
+    await blogPostIndex.getAllPosts();
+
+    const latestTwoPosts = blogPostIndex.getPostsAsItemTileItems(blogPostIndex.posts.slice(0, 2));
+    const leftoverPosts = blogPostIndex.getPostsAsItemTileItems(blogPostIndex.posts.slice(2));
+
+    const sidebarData = await Sidebar.getGenericSidebarData();
+
     return reply.view('home.ejs', {
       mainNavId: 'home',
+      latestTwoPosts,
+      leftoverPosts,
+      ...sidebarData,
     });
   });
 };
