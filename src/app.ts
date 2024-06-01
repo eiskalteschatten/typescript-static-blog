e358ab64b9e9ece3aa08ebc0770822318166c443
Alex Seifert <alex@alexseifert.com>
2024-06-01 21:57:08 +0200

    Register the renderer plugin

--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,12 @@ import type { FastifyCookieOptions } from '@fastify/cookie';
 import cookie from '@fastify/cookie';
 import ejs from 'ejs';
 import path from 'path';
+import minifier from 'html-minifier';
 
 import mainNav from './mainNav';
 import * as ejsHelpers from './lib/ejsHelpers';
-import Stats from './stats/Stats';
+// import Stats from './stats/Stats';
+import renderer from './lib/renderer';
 
 const port = Number(process.env.PORT) || 4000;
 
@@ -33,24 +35,24 @@ app.addHook('preHandler', (req: CustomRequest, reply: FastifyReply, done: DoneFu
     isDev: process.env.NODE_ENV === 'development',
   };
 
-  if (!req.url.match(Stats.excludeFromStats)) {
-    const noStatsParam = Number(req.query.noStats);
-    const noStats = req.cookies?.noStats === 'true' || noStatsParam === 1;
+  // if (!req.url.match(Stats.excludeFromStats)) {
+  //   const noStatsParam = Number(req.query.noStats);
+  //   const noStats = req.cookies?.noStats === 'true' || noStatsParam === 1;
 
-    if (noStatsParam === 1) {
-      reply.setCookie('noStats', 'true', {
-        path: '/',
-        // Expires in a year
-        expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365),
-      });
-    }
+  //   if (noStatsParam === 1) {
+  //     reply.setCookie('noStats', 'true', {
+  //       path: '/',
+  //       // Expires in a year
+  //       expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365),
+  //     });
+  //   }
 
-    if (!noStats) {
-      const stats = new Stats();
-      // Don't await this so that the request can be processed without waiting for the stats to be written
-      stats.addPageHit(req.url);
-    }
-  }
+  //   if (!noStats) {
+  //     const stats = new Stats();
+  //     // Don't await this so that the request can be processed without waiting for the stats to be written
+  //     stats.addPageHit(req.url);
+  //   }
+  // }
 
   done();
 });
@@ -65,7 +67,27 @@ app.register(fastifyView, {
   engine: { ejs },
   root: './templates',
   layout: '_layout.ejs',
+  propertyName: 'renderFullPage',
 });
+
+app.register(fastifyView, {
+  engine: { ejs },
+  root: './templates',
+  propertyName: 'renderPartial',
+  options: {
+    useHtmlMinifier: minifier,
+    htmlMinifierOptions: {
+      removeComments: true,
+      removeCommentsFromCDATA: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      removeAttributeQuotes: true,
+      removeEmptyAttributes: true,
+    },
+  },
+});
+
+app.register(renderer);
 
 app.register(helmet, {
   global: true,
